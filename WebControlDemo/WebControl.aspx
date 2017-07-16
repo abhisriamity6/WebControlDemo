9cedb49a685a48f9d9255c5544b4c5c7aeff5604
abhishek srivastava <abhi_sri_amity6@yahoo.com>
2017-07-16 16:05:18 +0530

    done

---
+++ WebControlDemo/WebControl.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebControl.aspx.cs" Inherits="WebControlDemo.WebControl" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title > Currency Converter</title>
+</head>
+<body>
+<form runat = "server">
+<div>
+Convert: &nbsp;
+<input type = "text" ID = "US" runat = "server" value=" 0" />
+&nbsp; U.S. dollars to Euros.
+<br /> <br />
+<input type = "submit" value = "OK" ID = "Convert" runat = "server"
+OnServerClick = "Convert_ServerClick" />
+<br /> <br />
+<p style = "font-weight: bold" ID = "Result" runat = "server" > </p>
+</div>
+</form>
+</body>
+</html>
---
+++ WebControlDemo/WebControl.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebControlDemo
+{
+    public partial class WebControl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Convert_ServerClick(object sender, EventArgs e)
+        {
+            decimal USAmount;
+            bool resultofcon = Decimal.TryParse(US.Value, out USAmount);
+            if (resultofcon)
+            {
+                decimal euroAmount = USAmount * 0.85M;
+                Result.InnerText = USAmount + " U.S. dollars = ";
+                Result.InnerText += euroAmount + " Euros.";
+            }
+            else
+            {
+                Result.InnerText = "The number you typed in was not in the " +"correct format. Use only numbers.";
+            }
+        }
+
+    }
+}
